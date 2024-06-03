4fc9c2720e15a8fdeba969221ddddda2855403eb
Sung Kil Han <sungkilhan221@gmail.com>
2024-06-03 16:01:04 -0400

    first

---
+++ app/(root)/page.tsx
@@ -0,0 +1,33 @@
+"use client";
+import PodcastCard from "@/components/PodcastCard";
+
+import { api } from "@/convex/_generated/api";
+import { useQuery } from "convex/react";
+
+const Home = () => {
+  const trendingPodcasts = useQuery(api.podcasts.getTrendingPodcasts);
+
+  return (
+    <div className="mt-9 flex flex-col gap-9 md:overflow-hidden">
+      <section className="flex flex-col gap-5">
+        <h1 className="text-20 font-bold text-white-1">Trending Podcasts</h1>
+
+        <div className="podcast_grid">
+          {trendingPodcasts?.map(
+            ({ _id, podcastTitle, podcastDescription, imageUrl }) => (
+              <PodcastCard
+                key={_id}
+                imgUrl={imageUrl!}
+                title={podcastTitle}
+                description={podcastDescription}
+                podcastId={_id}
+              />
+            )
+          )}
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default Home;
---
+++ convex/tasks.ts
@@ -0,0 +1,10 @@
+// @ts-nocheck
+
+import { query } from "./_generated/server";
+
+export const get = query({
+  args: {},
+  handler: async (ctx) => {
+    return await ctx.db.query("tasks").collect();
+  },
+});
